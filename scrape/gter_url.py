f7bbbf2540724ee9739bed33f9cbf40578dad265
yu1419 <865349796@qq.com>
2017-07-13 11:20:42 -0400

    update get url file, use functino instead of class

--- scrape/gter_url.py
+++ scrape/gter_url.py
@@ -4,30 +4,31 @@ import re
 from multiprocessing import Process, Lock, Queue
 import time
 
+url_head = ("http://bbs.gter.net/forum.php?"
+            "mod=forumdisplay&fid=49&orderby=dateline&"
+            "typeid=158&filter=author&orderby=dateline&typeid=158&page=")
+# sorted by posted date
 
-class Gter_scraper:
-    def __init__(self):
-        self.page_number = 1
-        self.max_page = 1
-        self.max_page_class = "last"
-        self.link_pattern = re.compile("normalthread_.*?")
-        self.link_class = "xst"
-        self.url_head = "http://bbs.gter.net/forum.php?mod=forumdisplay&fid=49&typeid=158&filter=typeid&typeid=158&page="
 
-    def scrape_one_page(self, page):
-        url = self.url_head + str(page)
-        html = urlopen(url, timeout=60)
-        bsObj = BeautifulSoup(html.read(), "html5lib")
-        all_url = []
-        all_content = bsObj.findAll("tbody", {"id": self.link_pattern})
-        for content in all_content:
-            try:
-                url_content = content.find("a", {"class": self.link_class})
-                if hasattr(url_content, "href"):
-                    all_url.append(url_content["href"])
-            except AttributeError as e:
-                print(e)
-        return all_url
+post_pattern = re.compile("normalthread_.*?")  # nomal post pattern
+
+url_class_name = "xst"  # url exsit in url_class_name <a>
+
+
+def scrape_one_page(connect_session, ):
+    url = url_head + str(page)
+    html = urlopen(url, timeout=60)
+    bsObj = BeautifulSoup(html.read(), "html5lib")
+    all_url = []
+    all_content = bsObj.findAll("tbody", {"id": self.link_pattern})
+    for content in all_content:
+        try:
+            url_content = content.find("a", {"class": self.link_class})
+            if hasattr(url_content, "href"):
+                all_url.append(url_content["href"])
+        except AttributeError as e:
+            print(e)
+    return all_url
 
     def get_total_page(self):
         page = 1
--- scrape/point_url.py
+++ scrape/point_url.py
@@ -1,32 +1,93 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import requests
+import time
 
 
-class Point_scraper:
-    def __init__(self):
-        self.page_number = 1
-        self.max_page = 1
-        self.thread_pattern = re.compile("normalthread_.*?")
-        self.class_name = "s xst"
-        self.url_head = "http://www.1point3acres.com/bbs/forum.php?mod=forumdisplay&fid=82&sortid=164&%1=&sortid=164&page="
+post_pattern = re.compile("normalthread_.*?")
+# normal link has a id start with normalthread
+# links stick to the top page do not have id start with normalthread
 
-    def scrape_page(self, page):
-        url = self.url_head + str(page)
-        html = urlopen(url, timeout=60)
-        bsObj = BeautifulSoup(html.read(), "html5lib")
-        if bsObj:
-            pattern = self.thread_pattern
-            all_content = bsObj.findAll("tbody", {"id": pattern})
-            class_name = self.class_name
-            url_list = []
-            for content in all_content:
-                try:
+url_class_name = "s xst"  # url exsit in url_class_name <a>
 
-                    url = content.find("a", {"class": class_name})["href"]
-                    if url:
-                        url = url.strip()
-                        url_list.append(url)
-                except AttributeError as e:
-                    print(e)
-            return url_list
+url_head = ("http://www.1point3acres.com/bbs/forum.php?"
+            "mod=forumdisplay&fid=82&orderby=dateline"
+            "&sortid=164&orderby=dateline&sortid=164"
+            "&filter=author&page=")
+# url head can find offer links ordered by published date
+
+
+s = requests.Session()
+
+
+def scrape_page(page, connect_session, url_head ,post_pattern, url_class_name):
+    # get offer links for each page
+
+    url = url_head + str(page)
+    html = s.get(url, timeout=60).content
+
+    bsObj = BeautifulSoup(html, "html5lib")
+    if bsObj:
+        pattern = post_pattern
+        all_content = bsObj.findAll("tbody", {"id": pattern})
+        url_list = []
+        for content in all_content:
+            try:
+                url = content.find("a", {"class": url_class_name})["href"]
+                if url:
+                    url = url.strip()
+                    url_list.append(url)
+            except AttributeError as e:
+                print(e)
+        return url_list
+
+
+def scrape_page_reconnect(page):  # get offer links for each page
+    url = url_head + str(page)
+    html = urlopen(url, timeout=60)
+    bsObj = BeautifulSoup(html.read(), "html5lib")
+    if bsObj:
+        pattern = thread_pattern
+        all_content = bsObj.findAll("tbody", {"id": pattern})
+        url_list = []
+        for content in all_content:
+            try:
+                url = content.find("a", {"class": url_class_name})["href"]
+                if url:
+                    url = url.strip()
+                    url_list.append(url)
+            except AttributeError as e:
+                print(e)
+        return url_list
+
+
+def get_tid(url):
+    # get tid from url
+    # url example: (http://www.1point3acres.com/bbs/forum.php?
+    #  mod=viewthread&tid=286466&extra=page%3D1%26filter%3
+    #  Dauthor%26orderby%3Ddateline%26sortid%3D164%26sortid
+    #  %3D164%26orderby%3Ddateline)
+
+    tid = re.search("tid=(\d+)", url).group(1)
+    return str(tid)
+
+if __name__ == "__main__":
+    url_head = ("http://bbs.gter.net/forum.php?"
+                "mod=forumdisplay&fid=49&orderby=dateline&"
+                "typeid=158&filter=author&orderby=dateline&typeid=158&page=")
+    # sorted by posted date
+
+    post_pattern = re.compile("normalthread_.*?")  # nomal post pattern
+
+    url_class_name = "xst"  # url exsit in url_class_name <a>
+    start_time = time.time()
+    for i in range(1, 10):
+        print(scrape_page(i, s, url_head, post_pattern, url_class_name))
+    stop_time = time.time()
+    print("connection: "+str(stop_time-start_time)+"\n")
+    start_time = time.time()
+    for i in range(1, 1):
+        (scrape_page_reconnect(i))
+    stop_time = time.time()
+    print("non_connection: "+str(stop_time-start_time))
