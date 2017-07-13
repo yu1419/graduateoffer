0e010628e9fc07f7a0d5b7c78649e3e6594cb217
yu1419 <865349796@qq.com>
2017-07-12 22:06:51 -0400

    add new model for sqlalchemy

--- scrape/get_db.py
+++ scrape/get_db.py
@@ -3,6 +3,7 @@ import pymysql
 
 
 def get_db():
+    """connect to mysql database, return db and cursor"""
     db = pymysql.connect("offer.cspfrhhjhhea.us-east-1.rds.amazonaws.com",
                          "master", "yusisheng123", "offer_1", charset='utf8')
     cursor = db.cursor()
--- scrape/sql_file/update_offercount.sql
+++ scrape/sql_file/update_offercount.sql
@@ -1,21 +1,12 @@
-drop trigger if exists update_offercount ;
+drop trigger if exists update_follow;
 DELIMITER $$
 
 CREATE
-    TRIGGER update_offercount after INSERT
-    ON offer
-    FOR EACH ROW 
+    TRIGGER update_follow after INSERT
+    ON user
+    FOR EACH ROW
     BEGIN
-		DECLARE total_cnt INT;
-        DECLARE has INT;
-        select count(*) into total_cnt from offer where offer.person_id = New.person_id and ranking is not NULL;
-        select count(*) into has from offer_count where person_id = New.person_id;
-        IF has THEN
-            update offer_count set total_count = total_cnt where person_id = New.person_id;
-        ELSE
-            insert into offer_count (total_count, person_id) values(total_cnt, New.person_id);
-        END IF;
-
-	END$$
+    insert into follow (user_id, followed) values (NEW.user_id, New.user_id);
+	  END$$
 
 DELIMITER ;
